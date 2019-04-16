94c341071f8e5246548563eb777a324cd0abf58a
srilatha ky <srilatha.y@thoughtworks.com>
2019-04-16 19:06:55 +0530

    Initial commit

---
+++ src/test/java/test_gradle/Test_One.java
@@ -0,0 +1,11 @@
+package test_gradle;
+
+
+        import org.testng.annotations.Test;
+
+public class Test_One {
+    @Test
+    public void testone(){
+        System.out.println("Hi Hello My first Gradle Project");
+    }
+}
